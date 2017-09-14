abdb96917b814256973354720032b3329339cf4b
Alvin Zhu <alvinz90@hotmail.com>
2017-09-14 10:17:18 -0400

    my-sbanking-ionic
    my-sbanking-ionic is a demo project of angular-ionic, showing the basic concepts of a mobile banking app using angular 4 / ionic 3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { AccountsummarytabsPage } from '../pages/accountsummarytabs/accountsummarytabs';
+import { AccountsummaryhomePage } from '../pages/accountsummaryhome/accountsummaryhome';
+import { AccountsummarycombinedbalancePage } from '../pages/accountsummarycombinedbalance/accountsummarycombinedbalance';
+import { TransferPage } from '../pages/transfer/transfer';
+import { BillsPage } from '../pages/bills/bills';
+import { EmtPage } from '../pages/emt/emt';
+import { WuPage } from '../pages/wu/wu';
+import { RdcPage } from '../pages/rdc/rdc';
+import { PrdsvcPage } from '../pages/prdsvc/prdsvc';
+import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +22,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+
+    LoginPage,
+    AccountsummarytabsPage,
+    AccountsummaryhomePage,
+    AccountsummarycombinedbalancePage,
+    TransferPage,
+    BillsPage,
+    EmtPage,
+    WuPage,
+    RdcPage,
+    PrdsvcPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -26,15 +42,25 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+
+    LoginPage,
+    AccountsummarytabsPage,
+    AccountsummaryhomePage,
+    AccountsummarycombinedbalancePage,
+    TransferPage,
+    BillsPage,
+    EmtPage,
+    WuPage,
+    RdcPage,
+    PrdsvcPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+
   ]
 })
 export class AppModule {}
